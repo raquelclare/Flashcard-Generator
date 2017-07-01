91a7729e2d115c4f83e2dccad0eb4ebeb3cdd6f3
Raquel Rodriguez <raquelclare@cpe-172-100-0-176.twcny.res.rr.com>
2017-06-30 23:27:07 -0500

    create function added and working as expected

--- ClozeCard.js
+++ ClozeCard.js
@@ -2,9 +2,9 @@ function ClozeCard (text, cloze) {
     this.fullText = text;
     this.cloze = cloze;
     // Partial should encompase the fullText minus cloze
-    this.partial = (this.fullText).replace((this.cloze), "...");
+    this.partial = (this.fullText).replace((this.cloze), "_______");
 
-    if (this.fullText.indexOf(this.cloze) == -1) {
+    if (this.fullText.indexOf(this.cloze) === -1) {
         // Must throw an error if does not exist, this is what -1 does
         console.log("Does not exist.")
     }
--- main.js
+++ main.js
@@ -4,27 +4,109 @@ var BasicCard = require("./BasicCard.js");
 // Takes in ClozeCard file
 var ClozeCard = require("./ClozeCard.js");
 
-// Creating flash cards
-var firstPresident = new BasicCard(
-    "Who was the first president of the United States?", "George Washington");
+var inquirer = require("inquirer");
+var flashcards = [];
 
-// "Who was the first president of the United States?"
-console.log(firstPresident.front); 
+// Testing flash cards
+// var firstPresident = new BasicCard(
+//     "Who was the first president of the United States?", "George Washington");
 
-// "George Washington"
-console.log(firstPresident.back); 
+// // "Who was the first president of the United States?"
+// console.log(firstPresident.front); 
 
-var firstPresidentCloze = new ClozeCard(
-    "George Washington was the first president of the United States.", "George Washington");
+// // "George Washington"
+// console.log(firstPresident.back); 
 
-// "George Washington"
-console.log(firstPresidentCloze.cloze); 
+// var firstPresidentCloze = new ClozeCard(
+//     "George Washington was the first president of the United States.", "George Washington");
 
-// " ... was the first president of the United States.
-console.log(firstPresidentCloze.partial); 
+// // "George Washington"
+// console.log(firstPresidentCloze.cloze); 
 
-// "George Washington was the first president of the United States.
-console.log(firstPresidentCloze.fullText);
+// // " ... was the first president of the United States.
+// console.log(firstPresidentCloze.partial); 
 
-// Should throw or log an error because "oops" doesn't appear in "This doesn't work"
-var brokenCloze = new ClozeCard("This doesn't work", "oops");
+// // "George Washington was the first president of the United States.
+// console.log(firstPresidentCloze.fullText);
+
+// // Should throw or log an error because "oops" doesn't appear in "This doesn't work"
+// var brokenCloze = new ClozeCard("This doesn't work", "oops");
+
+function create() {
+    inquirer.prompt ([
+
+        {
+            type: "list",
+            message: "Do you want to make basic flash cards or cloze flashcards?",
+            choices: ["Basic flashcards", "Cloze flashcards"],
+            name: "flashcardType"
+
+        }
+    ]).then(function(user) {
+        if (user.flashcardType === "Basic flashcards"){
+            console.log("Creating a basic flashcard...");
+
+            inquirer.prompt([
+
+                {
+                    type: "input",
+                    message: "Flashcard question:", 
+                    name: "front"
+
+                },
+                {
+
+                    type: "input",
+                    message: "Flashcard answer:",
+                    name: "back"
+                }
+            ]).then(function(basicUser){
+
+                var flashcard = new BasicCard(basicUser.front, basicUser.back);
+
+                flashcards.push(flashcard);
+
+                console.log("New flashcard created!");
+
+            });
+
+        } else if (user.flashcardType === "Cloze flashcards") {
+            console.log("Creating a cloze-deleted flashcard...");
+
+            inquirer.prompt([
+
+                {
+                    type: "input",
+                    message: "Flashcard fact: (ex. George Washington was the first president of the United States)", 
+                    name: "text"
+
+                },
+                {
+
+                    type: "input",
+                    message: "Which part of the fact you would like to omit:",
+                    name: "cloze"
+                }
+            ]).then(function(clozeUser){
+
+                var flashcard = new ClozeCard(clozeUser.text, clozeUser.cloze);
+
+                flashcards.push(flashcard);
+
+                console.log(flashcard);
+
+                console.log("New flashcard created!");
+
+            });
+        }
+    });
+}
+
+create();
+
+// Will need a log.txt file for this
+function displayFlashcards() {
+    return console.log(flashcards);
+}
+
+displayFlashcards();
